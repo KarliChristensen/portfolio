d8a126b02aa3821b8a296ae1e1792f88d3eba9b6
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-10-16 11:30:21 +0200

    Snapping established

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
-    <div className='h-screen overflow-auto snap-mandatory snap-always'>
+    <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+      <section className="h-screen snap-start  bg-red-400">
+        <h1>Page 1</h1>
+      </section>
+      <section className="h-screen snap-start  bg-blue-500">
+        <h1> Page 2</h1>
+      </section>
+      <section className="h-screen snap-start  bg-green-500">
+        <h1> Page 3</h1>
+      </section>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
