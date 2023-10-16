81ac0550cab291523635fa24ae7c073a43b3727a
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-10-16 11:50:22 +0200

    Navbar transparent

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <nav className="sticky top-0" >
-      <div className="flex justify-between px-16 py-8">
+    <header className="sticky top-0 bg-transparent z-50 -mt-20">
+      <div className="flex justify-between px-16 py-7">
         <div className="flex justify-between w-1/3">
-          <Link href="/about">ABOUT</Link>
+          <Link href="/">HOME</Link>
           <Link href="/work">WORK</Link>
           <Link href="/contact">CONTACT</Link>
         </div>
@@ -16,7 +16,7 @@ function Navbar() {
           <p>DIGITAL PORTFOLIO</p>
         </div>
       </div>
-    </nav>
+    </header>
   );
 }
 
