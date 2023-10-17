a02364d427602e475d0952b370eaba444d112738
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-10-18 01:44:54 +0200

    Navbar responsive

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,15 +5,15 @@ import Link from "next/link";
 
 function Navbar() {
   return (
-    <header className="sticky top-0 bg-transparent z-50 -mt-20">
-      <div className="flex justify-between px-16 py-7">
-        <div className="flex justify-between w-1/3">
-          <Link href="/">HOME</Link>
-          <Link href="/work">WORK</Link>
-          <Link href="/contact">CONTACT</Link>
+    <header className="absolute top-0 bg-transparent w-full z-50">
+      <div className="flex justify-evenly md:justify-between px-10 py-7">
+        <div className="flex justify-evenly w-screen md:w-auto">
+          <a className="hover:text-purple-600 nav-text" href="#about">ABOUT</a>
+          <a className="hover:text-purple-600 nav-text" href="#work">WORK</a>
+          <a className="hover:text-purple-600 nav-text" href="#contact">CONTACT</a>
         </div>
-        <div>
-          <p>DIGITAL PORTFOLIO</p>
+        <div className="hidden md:block">
+          <p className="nav-text">DIGITAL PORTFOLIO</p>
         </div>
       </div>
     </header>
